f6fd423ed10beae03d836769c98a8dbf4fe30b25
Himanshu Lal <himanshulal56@gmail.com>
2024-04-25 11:12:12 +0530

    modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,5 +1,24 @@
 'use strict';
-const takeInput = require('readline-sync');
-console.log("Enter the string")
-let a = takeInput.question();
-console.log(a);
+const modal = document.querySelector('.modal');
+const modalBtn = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const hideModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < modalBtn.length; i++) {
+  modalBtn[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+closeModalBtn.addEventListener('click', hideModal);
+overlay.addEventListener('click', hideModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    hideModal();
+    console.log('esc pressed');
+  }
+});
